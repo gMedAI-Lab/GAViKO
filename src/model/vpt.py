fccac190d2e360927c5ab4eeba233c9bd0471c82
DanhNguyennene <danhvm12345@gmail.com>
2025-07-07 16:47:38 +0700

    configed

--- src/model/vpt.py
+++ src/model/vpt.py
@@ -77,7 +77,7 @@ class Transformer(nn.Module):
 
         return self.norm(x)
 
-class VisualPromptTuning(nn.Module):
+class VisionTransformer(nn.Module):
     def __init__(self,
                  *,
                  image_size,
@@ -212,3 +212,159 @@ class VisualPromptTuning(nn.Module):
 
         x = self.to_latent(x)
         return self.mlp_head(x)
+    
+class PromptedVisionTransformer(nn.Module):
+    def __init__(self,
+                 image_size: int,
+                 image_patch_size: int,
+                 frames: int,
+                 frame_patch_size : int,
+                 num_layers: int,
+                 num_heads: int,
+                 hidden_dim: int,
+                 mlp_dim: int,
+                 dropout: 0.0,
+                 emb_dropout: 0.0,
+                 num_classes = 5,
+                 channels = 3,
+                 dim_head = 64,
+                 freeze_vit = True,
+                 pool = 'cls',
+                 pretrain_path = None,
+                 prompt_dropout = 0.0,
+                 prompt_dim = 64,
+                 num_prompts = 8,
+                 deep_prompt = True,
+                 ):
+        super().__init__()
+        self.image_size = image_size
+        self.num_layers = num_layers
+        self.image_patch_size = image_patch_size
+        self.hidden_dim = hidden_dim
+        self.mlp_dim = mlp_dim
+        self.emb_dropout = emb_dropout
+        self.dropout = dropout
+        self.num_classes = num_classes
+        self.deep_prompt = deep_prompt
+
+        self.prompt_proj = nn.Linear(prompt_dim, hidden_dim)
+        self.prompt_dropout = nn.Dropout(prompt_dropout)
+
+        if self.deep_prompt:
+            self.deep_prompt_embeddings = nn.Parameter(torch.zeros(
+                    num_layers, num_prompts, prompt_dim))
+            # xavier_uniform initialization
+            nn.init.xavier_uniform_(self.deep_prompt_embeddings.data)
+        else:
+            self.prompt_embeddings = nn.Parameter(torch.zeros(
+                    1, num_prompts, prompt_dim))
+             # xavier_uniform initialization
+            nn.init.xavier_uniform_(self.prompt_embeddings.data)
+
+        self.vision_transformer = VisionTransformer(
+                                     image_size=image_size,
+                                     image_patch_size=image_patch_size,
+                                     frames=frames,
+                                     frame_patch_size=frame_patch_size,
+                                     num_classes=num_classes,
+                                     dim=hidden_dim,
+                                     depth=num_layers,
+                                     heads=num_heads,
+                                     mlp_dim=mlp_dim,
+                                     pool = pool,
+                                     channels = channels,
+                                     dim_head = dim_head,
+                                     dropout = dropout,
+                                     emb_dropout = emb_dropout,
+                                     pretrain_path=pretrain_path)
+        self.freeze_vit = freeze_vit
+
+        # self.init_head_weights()
+        self.init_promptproj_weights()
+
+        if self.freeze_vit:
+            for k, p in self.vision_transformer.named_parameters():
+                if ("transformer" in k or "cls_token" in k or "conv_proj" in k or "pos_embedding" in k):
+                    p.requires_grad = False
+
+    def init_head_weights(self):
+        nn.init.xavier_uniform_(self.vision_transformer.mlp_head.weight)
+        nn.init.zeros_(self.vision_transformer.mlp_head.bias)
+        print("Initialize head weight successfully!")
+
+    def init_promptproj_weights(self):
+        nn.init.xavier_uniform_(self.prompt_proj.weight)
+        nn.init.zeros_(self.prompt_proj.bias)
+        print("Initialize prompt projector successfully!")
+
+    def train(self, mode=True):
+        if mode:
+            # Ensure ViT encoder stays in eval mode if frozen
+            super().train(mode)
+            if self.freeze_vit:
+                self.vision_transformer.transformer.eval()
+                self.vision_transformer.conv_proj.eval()
+                self.vision_transformer.dropout.eval()
+                self.vision_transformer.mlp_head.train()
+                self.prompt_proj.train()
+        else:
+            # Set all submodules to evaluation mode
+            for module in self.children():
+                module.eval()
+
+    def _process_input(self, x: torch.Tensor) -> torch.Tensor:
+        return self.vision_transformer.conv_proj(x)
+
+    def forward_prompt(self, x):
+        B = x.shape[0] # (batch_size, 1 + n_patches, hidden_dim)
+        # after CLS token, all before image patches
+        x = torch.cat((
+                x[:, :1, :],
+                self.prompt_dropout(self.prompt_proj(self.prompt_embeddings).expand(B, -1, -1)),
+                x[:, 1:, :]
+            ), dim=1)
+        # (batch_size, cls_token + n_prompt + n_patches, hidden_dim)
+        for i in range(self.num_layers):
+            attn, ff = self.vision_transformer.transformer.layers[i]
+            x = attn(x) + x
+            x = ff(x) + x
+        x = self.vision_transformer.transformer.norm(x)
+        x = x.mean(dim=1) if self.vision_transformer.pool == 'mean' else x[:, 0]
+        x = self.vision_transformer.to_latent(x)
+        return self.vision_transformer.mlp_head(x)
+
+    def forward_deep_prompt(self, x):
+        num_layers = self.num_layers
+        B = x.shape[0]
+        for i in range(num_layers):
+            if i == 0:
+                x = torch.cat((x[:,:1,:],
+                               self.prompt_dropout(self.prompt_proj(self.deep_prompt_embeddings[i].expand(B, -1, -1))),
+                               x[:,1:,:]), dim=1) # Combine prompt embeddings with image embeddings (after cls tokens before patch embeddings) at the first Transformer layer
+            else:
+                x = torch.cat((x[:,:1,:],
+                               self.prompt_dropout(self.prompt_proj(self.deep_prompt_embeddings[i].expand(B, -1, -1))),
+                               x[:,(1 + self.deep_prompt_embeddings[i].shape[1]):,:]), dim=1) # after cls tokens & prompt embeddings at the current layers (to avoid overiding prompt embedding in previous, help keep info from current layers) before patch embeddings
+
+            attn, ff = self.vision_transformer.transformer.layers[i]
+            x = attn(x) + x
+            x = ff(x) + x
+        x = self.vision_transformer.transformer.norm(x)
+        x = x.mean(dim=1) if self.vision_transformer.pool == 'mean' else x[:, 0]
+        x = self.vision_transformer.to_latent(x)
+        return self.vision_transformer.mlp_head(x)
+
+
+    def forward(self, x: torch.Tensor):
+        x = self._process_input(x)
+        x = x.flatten(2).transpose(1,2) # [B, N, C]
+        b, n, _ = x.shape
+
+        cls_tokens = repeat(self.vision_transformer.cls_token, '1 1 d -> b 1 d', b = b)
+        x = torch.cat((cls_tokens, x), dim=1)
+        x += self.vision_transformer.pos_embedding[:, :(n + 1)]
+        x = self.vision_transformer.dropout(x)
+        if self.deep_prompt==True:
+            return self.forward_deep_prompt(x)
+        else:
+            return self.forward_prompt(x)
--- src/train.py
+++ src/train.py
@@ -2,7 +2,14 @@ import torchio as tio
 import pandas as pd
 from data.dataset import CustomDataset
 from torch.utils.data import DataLoader
-from model.gaviko import VisionTransformer
+from model.gaviko import Gaviko
+from model.adaptformer import AdaptFormer
+from model.bifit import BiFit
+from model.dvpt import DynamicVisualPromptTuning
+from model.evp import ExplicitVisualPrompting
+from model.ssf import ScalingShiftingFeatures
+from model.melo import MedicalLoRA
+from model.vpt import PromptedVisionTransformer
 import torch
 import logging
 import os
@@ -61,15 +68,16 @@ class DataPreprocessor:
         val_ds = CustomDataset(val_df, transforms=val_transforms, image_folder=self.config['data']['image_folder'])
         test_ds = CustomDataset(test_df, transforms=test_transforms, image_folder=self.config['data']['image_folder'])
 
-        train_loader = DataLoader(train_ds, batch_size=self.config['model']['batch_size'], shuffle=True, num_workers=self.config['data']['num_workers'], pin_memory=True)
-        test_loader = DataLoader(test_ds, batch_size=self.config['model']['batch_size'], shuffle=False, num_workers=self.config['data']['num_workers'], pin_memory=True)
-        val_loader = DataLoader(val_ds, batch_size=self.config['model']['batch_size'], shuffle=False, num_workers=self.config['data']['num_workers'], pin_memory=True)
+        train_loader = DataLoader(train_ds, batch_size=self.config['data']['batch_size'], shuffle=True, num_workers=self.config['data']['num_workers'], pin_memory=True)
+        test_loader = DataLoader(test_ds, batch_size=self.config['data']['batch_size'], shuffle=False, num_workers=self.config['data']['num_workers'], pin_memory=True)
+        val_loader = DataLoader(val_ds, batch_size=self.config['data']['batch_size'], shuffle=False, num_workers=self.config['data']['num_workers'], pin_memory=True)
         return train_loader, val_loader, test_loader, train_ds, val_ds, test_ds
 
 def train(config):
     # Initialize WandB
     os.makedirs(config['utils']['log_dir'], exist_ok=True)
-    csv_logger = CSVLogger(log_dir=config['utils']['log_dir'], filename_prefix='vit_train', 
+    model_name = config['model']['model_type']
+    csv_logger = CSVLogger(log_dir=config['utils']['log_dir'], filename_prefix=f'{model_name}_training_log', 
                        fields=['epoch', 'train_step_acc', 'train_step_loss', 'train_epoch_loss', 
                                'val_step_acc', 'val_step_loss', 'val_epoch_loss', 'lr', 
                                'best_epoch', 'best_val_acc', 'time_stamp', 'train_step', 'val_step','train_epoch_acc', 'val_epoch_acc'])
@@ -90,53 +98,200 @@ def train(config):
     # Preprocess data
     data_preprocessor = DataPreprocessor(config)
     train_loader, val_loader, test_loader, train_ds, val_ds, test_ds = data_preprocessor.preprocess(pd.read_csv(config['data']['data_path']))
-    model = VisionTransformer(
-        image_size=config['model']['image_size'],
-        image_patch_size=config['model']['image_patch_size'],
-        frames = config['model']['frames'],
-        frame_patch_size = config['model']['frame_patch_size'],
-        depth=config['model']['depth'],
-        heads=config['model']['heads'],
-        dim=config['model']['dim'],
-        mlp_dim=config['model']['mlp_dim'],
-        dropout=config['model']['dropout'],
-        emb_dropout=config['model']['emb_dropout'],
-        attn_drop=config['model']['attn_drop'],
-        proj_drop=config['model']['proj_drop'],
-        channels=config['model']['channels'],
-        num_classes=config['model']['num_classes'],
-        freeze_vit = config['model']['freeze_vit'],
-        pool = config['model']['pool'],
-        pretrain_path = config['model']['pretrain_path'],
-        num_prompts=config['model']['num_prompts'],
-        prompt_latent_dim=config['model']['prompt_latent_dim'],
-        local_dim=config['model']['local_dim'],
-        local_k= tuple(config['model']['local_k']),
-        DHW=tuple(config['model']['DHW']),
-        share_factor=config['model']['share_factor']
-    )
+
+    # Initialize model
+
+    if config['model']['model_type'] == 'gaviko':
+        model = Gaviko(
+            image_size=config['model']['image_size'],
+            image_patch_size=config['model']['image_patch_size'],
+            frames = config['model']['frames'],
+            frame_patch_size = config['model']['frame_patch_size'],
+            heads=config['model']['heads'],
+            dim=config['model']['dim'],
+            mlp_dim=config['model']['mlp_dim'],
+            dropout=config['model']['dropout'],
+            emb_dropout=config['model']['emb_dropout'],
+            attn_drop=config['model']['attn_drop'],
+            proj_drop=config['model']['proj_drop'],
+            channels=config['model']['channels'],
+            num_classes=config['model']['num_classes'],
+            freeze_vit = config['model']['freeze_vit'],
+            pool = config['model']['pool'],
+            pretrain_path = config['model']['pretrain_path'],
+            num_prompts=config['model']['num_prompts'],
+            prompt_latent_dim=config['model']['prompt_latent_dim'],
+            local_dim=config['model']['local_dim'],
+            local_k= tuple(config['model']['local_k']),
+            DHW=tuple(config['model']['DHW']),
+            share_factor=config['model']['share_factor']
+        )
+
+    elif config['model']['model_type'] == 'adaptformer':
+        model = AdaptFormer(
+            image_size=config['model']['image_size'],
+            image_patch_size=config['model']['image_patch_size'],
+            frames = config['model']['frames'],
+            frame_patch_size = config['model']['frame_patch_size'],
+            depth=config['model']['depth'],
+            heads=config['model']['heads'],
+            dim=config['model']['dim'],
+            mlp_dim=config['model']['mlp_dim'],
+            dropout=config['model']['dropout'],
+            emb_dropout=config['model']['emb_dropout'],
+            channels = config['model']['channels'],
+            num_classes = config['model']['num_classes'],
+            freeze_vit = config['model']['freeze_vit'],
+            pool = config['model']['pool'],
+            pretrain_path = config['model']['pretrain_path'],
+        )# .to(device)
+
+    elif config['model']['model_type'] == 'bifit':
+        model = BiFit(
+            image_size=config['model']['image_size'],
+            image_patch_size=config['model']['image_patch_size'],
+            frames = config['model']['frames'],
+            frame_patch_size = config['model']['frame_patch_size'],
+            depth=config['model']['depth'],
+            heads=config['model']['heads'],
+            dim=config['model']['dim'],
+            mlp_dim=config['model']['mlp_dim'],
+            dropout=config['model']['dropout'],
+            emb_dropout=config['model']['emb_dropout'],
+            channels = config['model']['channels'],
+            num_classes = config['model']['num_classes'],
+            pool = config['model']['pool'],
+            pretrain_path = config['model']['pretrain_path'],
+        )
+        for key, value in model.named_parameters():
+            if "bias" in key:
+                value.requires_grad = True
+            elif "head" in key:
+                value.requires_grad = True
+            else:
+                value.requires_grad = False
+    elif config['model']['model_type'] == 'dvpt':
+        model = DynamicVisualPromptTuning(
+            image_size=config['model']['image_size'],
+            image_patch_size=config['model']['image_patch_size'],
+            frames = config['model']['frames'],
+            frame_patch_size = config['model']['frame_patch_size'],
+            depth=config['model']['depth'],
+            heads= config['model']['heads'],
+            dim= config['model']['dim'],
+            mlp_dim=config['model']['mlp_dim'],
+            dropout=config['model']['dropout'],
+            emb_dropout=config['model']['emb_dropout'],
+            channels = config['model']['channels'],
+            num_classes = config['model']['num_classes'],
+            freeze_vit = config['model']['freeze_vit'],
+            pool = config['model']['pool'],
+            pretrain_path = config['model']['pretrain_path'],
+            num_prompts = config['model']['num_prompts'],
+        )# .to(device)
+
+    elif config['model']['model_type'] == 'evp':
+        model = ExplicitVisualPrompting(
+            image_size=config['model']['image_size'],
+            image_patch_size=config['model']['image_patch_size'],
+            frames = config['model']['frames'],
+            frame_patch_size = config['model']['frame_patch_size'],
+            depth=config['model']['depth'],
+            heads=config['model']['heads'],
+            dim=config['model']['dim'],
+            mlp_dim=config['model']['mlp_dim'],
+            dropout=config['model']['dropout'],
+            emb_dropout=config['model']['emb_dropout'],
+            channels = config['model']['channels'],
+            num_classes = config['model']['num_classes'],
+            pool = config['model']['pool'],
+            pretrain_path=config['model']['pretrain_path'],
+            freeze_vit=config['model']['freeze_vit'],
+            scale_factor=config['model']['scale_factor'],
+            input_type=config['model']['input_type'],
+            freq_nums=config['model']['freq_nums'],
+            handcrafted_tune=config['model']['handcrafted_tune'],
+            embedding_tune=config['model']['embedding_tune'],
+        )# .to(device)
+
+    elif config['model']['model_type'] == 'ssf':
+        model = ScalingShiftingFeatures(
+            image_size=config['model']['image_size'],
+            image_patch_size=config['model']['image_patch_size'],
+            frames = config['model']['frames'],
+            frame_patch_size = config['model']['frame_patch_size'],
+            depth=config['model']['depth'],
+            heads=config['model']['heads'],
+            dim=config['model']['dim'],
+            mlp_dim=config['model']['mlp_dim'],
+            dropout=config['model']['dropout'],
+            emb_dropout=config['model']['emb_dropout'],
+            channels = config['model']['channels'],
+            num_classes = config['model']['num_classes'],
+            freeze_vit = config['model']['freeze_vit'],
+            pool = config['model']['pool'],
+            pretrain_path = config['model']['pretrain_path'],
+        )
+    elif config['model']['model_type'] == 'melo':
+        model = MedicalLoRA(
+            image_size=config['model']['image_size'],
+            image_patch_size=config['model']['image_patch_size'],
+            frames=config['model']['frames'],
+            frame_patch_size=config['model']['frame_patch_size'],
+            depth=config['model']['depth'],
+            heads=config['model']['heads'],
+            dim=config['model']['dim'],
+            mlp_dim=config['model']['mlp_dim'],
+            dropout=config['model']['dropout'],
+            emb_dropout=config['model']['emb_dropout'],
+            channels = config['model']['channels'],
+            num_classes = config['model']['num_classes'],
+            pool = config['model']['pool'],
+            pretrain_path = config['model']['pretrain_path'],
+        )
+    elif config['model']['model_type'] == 'deep_vpt' or config['model']['model_type'] == 'shallow_vpt':
+        model = PromptedVisionTransformer(
+            image_size=config['model']['image_size'],
+            image_patch_size=config['model']['image_patch_size'],
+            frames = config['model']['frames'],
+            frame_patch_size = config['model']['frame_patch_size'],
+            num_layers=config['model']['num_layers'],
+            num_heads=config['model']['num_heads'],
+            hidden_dim=config['model']['hidden_dim'],
+            mlp_dim=config['model']['mlp_dim'],
+            dropout=config['model']['dropout'],
+            emb_dropout=config['model']['emb_dropout'],
+            channels = config['model']['channels'],
+            num_classes = config['model']['num_classes'],
+            freeze_vit = config['model']['freeze_vit'],
+            pool = config['model']['pool'],
+            pretrain_path = config['model']['pretrain_path'],
+            num_prompts = config['model']['num_prompts'],
+            prompt_dropout = config['model']['prompt_dropout'],
+            prompt_dim = config['model']['prompt_dim'],
+            deep_prompt=config['model']['deep_prompt']
+        )
     model.to(device)
 
     count_freeze = 0
     count_tuning = 0
-    # tuning_params = []
-    # freeze_params = []
+    tuning_params = []
+    freeze_params = []
     for name, param in model.named_parameters():
         # if 'prompt' in name or 'mlp_head' in name:
         if param.requires_grad == True:
             count_tuning += 1
             # print(name, param.shape)
+            tuning_params.append(name)
         else:
             count_freeze += 1
+            freeze_params.append(name)
             logging.info(f'Freeze param: {name}')
-    logging.info(f'Total frozen parameters: {count_freeze}')
-    logging.info(f'Total parameters: {count_freeze + count_tuning}')
+    logging.info(f'There are {count_tuning} trainable params.')
+    logging.info(f'including: {tuning_params}')
+    logging.info(f'There are {count_freeze} freeze params')
     total_params = sum(p.numel() for p in model.parameters() if p.requires_grad)
     logging.info(f'Total trainable parameters: {total_params}')
-
-
-
-
     # alpha = torch.FloatTensor([2.65, 5.39, 3.83, 7.03, 29.67]).to(device)
     criterion = FocalLoss(gamma=1.2)
 
@@ -160,8 +315,7 @@ def train(config):
         three_phase=config['train']['scheduler']['three_phase']  # không dùng 3 giai đoạn (chỉ dùng 2: lên-xuống)
     )
 
-
-    val_acc_max = 0.0
+    val_acc_max =0
 
     current_epoch = 0
 
@@ -322,11 +476,12 @@ def train(config):
             val_acc_max = val_acc
             if val_acc_max > config['train']['save_threshold']:
                 logging.info("Saving model ...")
-                model_path = os.path.join(save_dir, f'best_model_epoch{current_epoch}_acc{val_acc:.4f}.pt')
+                model_name = config['model']['model_type']
+                save_dir = os.path.join(config['train']['save_dir'],'experiments', model_name)
+                os.makedirs(save_dir, exist_ok=True)
+                model_path = os.path.join(save_dir, f'{model_name}_best_model_epoch{current_epoch}_acc{val_acc:.4f}.pt')
                 torch.save(model.state_dict(), model_path)
 
-                save_dir = config['train']['save_dir']
-                os.makedirs(save_dir, exist_ok=True)
                 logging.info(f"Model saved to {model_path}")
             epoch_since_improvement = 0
         else:
@@ -342,8 +497,8 @@ def train(config):
     logging.info("Training completed.")
     #save log
     # Update WandB summary
-    wandb.run.summary['best_val_acc'] = val_acc_max
-    wandb.run.summary['best_epoch'] = best_epoch
+    # wandb.run.summary['best_val_acc'] = val_acc_max
+    # wandb.run.summary['best_epoch'] = best_epoch
 
     # Finish the run
     wandb.finish()
@@ -353,9 +508,19 @@ def main():
     parser = argparse.ArgumentParser(description="Training script for Gaviko model")
     parser.add_argument('--config', type=str, default='configs/original_gaviko.yaml',
                         help='Path to the configuration file')
+    parser.add_argument('--model', type=str, default='gaviko',
+                        choices=['gaviko', 'adaptformer', 'bifit', 'dvpt', 'evp', 'ssf', 'melo', 'deep_vpt','shallow_vpt'],
+                        help='Model to train: gaviko, adaptformer, bifit, dvpt, evp, ssf, melo, deep_vpt, shallow_vpt')
+    parser.add_argument('--wandb',default=None, help='Enable WandB logging')
     args = parser.parse_args()
 
     config = OmegaConf.load(args.config)
+    config['wandb']['enable'] = args.wandb if args.wandb is not None else config['wandb']['enable']
+    config['model']['model_type'] = args.model 
+    if config['model']['model_type'] == 'deep_vpt':
+        config['model']['deep_prompt'] = True
+    elif config['model']['model_type'] == 'shallow_vpt':
+        config['model']['deep_prompt'] = False
     logging.info(f"Config: {config}")
     train(config)
 
